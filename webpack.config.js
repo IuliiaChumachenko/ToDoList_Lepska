df14f251231467a87f7d06cf1b03be1f41fe6ed8
Yuliia Lepska <iuliia.chumachenko@gmail.com>
2018-06-01 13:05:25 +0300

    configurated webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,53 @@
 const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
     entry: [
         './src/js/script.js',
+        './src/scss/style.scss'
     ],
     output: {
         filename: './bundle.js'
     },
     devtool: "source-map",
     module: {
-        rules: [{
-            test: /\.js$/,
-            include: path.resolve(__dirname, 'src/js'),
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: 'env'
+            rules: [{
+                test: /\.js$/,
+                include: path.resolve(__dirname, 'src/js'),
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: 'env'
+                    }
                 }
-            }
-        },
+            },
+            {
+                test: /\.(sass|scss)$/,
+                include: path.resolve(__dirname, 'src/scss'),
+                use: ExtractTextPlugin.extract({
+                    use: [{
+                        loader: "css-loader",
+                        options: {
+                            sourceMap: true,
+                            minimize: true,
+                            url: false
+                        }
+                    },
+                        {
+                            loader: "sass-loader",
+                            options: {
+                                sourceMap: true
+                            }
+                        }
+                    ]
+                })
+            },
         ]
     },
     plugins: [
+        new ExtractTextPlugin({
+            filename: './style.bundle.css',
+            allChunks: true,
+        }),
     ]
 };
